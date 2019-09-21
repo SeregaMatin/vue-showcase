c6025cfa0939691ada686220649750eb228c2937
Serega Matin <seregamatin@gmail.com>
2019-09-21 18:13:20 +0500

    Generate initial application

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Set right publicPath for GitHub pages (see https://cli.vuejs.org/ru/guide/deployment.html#github-pages).
+  // GitHub pages address is https://seregamatin.github.io/vue-showcase/ so right publicPath is '/vue-showcase/'.
+  // GitHub pages deployment is done by TravicCI (see .travis.yml file at root of the repo).
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-showcase/'
+    : '/',
+};
